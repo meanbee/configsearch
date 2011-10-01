dda8accbca0756451abb6e58abe8424502a9c01d
Nick Jones <nick@nicksays.co.uk>
2011-10-01 17:37:27 +0100

    Initial commit

---
+++ app/code/community/Meanbee/Config/Block/Search.php
@@ -0,0 +1,6 @@
+<?php
+class Meanbee_Config_Block_Search extends Mage_Adminhtml_Block_Template {
+    public function _construct() {
+        $this->setTemplate('meanbee/config/search.phtml');
+    }
+}
---
+++ app/code/community/Meanbee/Config/controllers/SearchController.php
@@ -0,0 +1,83 @@
+<?php
+class Meanbee_Config_SearchController extends Mage_Adminhtml_Controller_Action {
+    public function indexAction() {
+        $search_param = $this->_getSearchParameter();
+        
+        if ($search_param) {
+            $fields = $this->_loadCachedConfigArray();
+            
+            $results = array();
+            
+            foreach ($fields as $field) {
+                $description = $field['description'];
+                
+                if (strpos($description, $search_param) !== false) {
+                    $url = $this->getUrl('adminhtml/system_config/edit/section/' . $field['section']) . '?fieldset=' . $field['fieldset_id'] . '&row=' . $field['row_id'];
+                    $results[] = "<a href='" . $url . "'>" . $field['description'] . "</a>";
+                }
+            }
+            
+            // @TODO Actually perform the search..
+            echo $this->_return($results);
+        } else {
+            echo $this->_return('You failed to provide a search parameter');
+        }
+    }
+
+    /**
+     * @TODO Actually cache it..
+     */
+    protected function _loadCachedConfigArray() {
+        $fields = array();
+        $config = Mage::getSingleton('adminhtml/config');
+
+        foreach ($config->getSections() as $tab) {
+            foreach ($tab->children() as $section) {
+                $section_label = (string) $section->label;
+                $section_tag = $section->getName();
+
+                foreach ($section->descend('groups') as $groupgroup) {
+                    foreach ($groupgroup->children() as $group) {
+                        $group_label = (string) $group->label;
+                        $group_tag = $group->getName();
+
+                        foreach ($group->descend('fields') as $fieldgroup) {
+                            foreach ($fieldgroup->children() as $field) {
+                                $field_label = $field->label;
+                                $field_tag = $field->getName();
+
+                                $fields[] = array(
+                                    'section'     => $section_tag,
+                                    'fieldset_id' => "{$section_tag}_{$group_tag}",
+                                    'row_id'      => "{$section_tag}_{$group_tag}_{$field_tag}",
+                                    'description' => "$section_label: $group_label: $field_label"
+                                );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+            
+        return $fields;
+    }
+
+    protected function _getSearchParameter() {
+        return $this->getRequest()->getParam('q');
+    }
+
+    protected function _return($messages) {
+      if (!is_array($messages)) {
+          $messages = array($messages);
+      }
+      
+      echo '<ul>';
+      
+      foreach ($messages as $message) {
+          echo "<li>$message</li>";
+      }
+      
+      echo '</ul>';
+    }
+}
+
